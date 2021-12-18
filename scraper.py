1533fe26047ffcdaae9347a4303a5821169c0c93
Jeff Cheng <jeffcheng777@gmail.com>
2021-12-19 00:02:34 +0800

    base version

---
+++ scraper.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+all_quotes = []
+res = requests.get("http://quotes.toscrape.com")
+soup = BeautifulSoup(res.text)
+quotes = soup.find_all(class_="quote")
+for quote in quotes:
+    all_quotes.append({
+        "text": quote.find(class_="text").get_text(),
+        "author": quote.find(class_="author").get_text()
+    })
